9baae3e037e2cf130426fa397159917142807746
Marty Trujillo <marty@ilikeemail.com>
2024-01-31 12:11:31 -0500

    converts local variables to class variables.

--- src/app/threejs.service.ts
+++ src/app/threejs.service.ts
@@ -8,40 +8,45 @@ import { BoxGeometry, Mesh, MeshNormalMaterial, Object3DEventMap, PerspectiveCam
 })
 export class ThreejsService {
 
+  width = 0;
+  height = 0;
+  camera: PerspectiveCamera = new THREE.PerspectiveCamera();
+  meshes: Mesh<BoxGeometry, MeshNormalMaterial, Object3DEventMap>[] = [];
+  renderer: WebGLRenderer = new THREE.WebGLRenderer( { antialias: true } );
+
   constructor() { }
 
   runThreeJs(window: Window): void
   {
-    const width = window.innerWidth, height = window.innerHeight;
 
-// init
+    const animation = ( time: number ) => {
 
-const camera: PerspectiveCamera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-camera.position.z = 5;
+      this.meshes.forEach(
+        (mesh) => {
+          mesh.rotation.x = time / 2000;
+          mesh.rotation.y = time / 1000;
+        }
+      );
+      this.renderer.render( scene, this.camera );
+    
+    }
 
-const scene: Scene = new THREE.Scene();
+    this.width = window.innerWidth
+    this.height = window.innerHeight;
 
-const geometry: BoxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material: MeshNormalMaterial = new THREE.MeshNormalMaterial();
+    this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
+    this.camera.position.z = 5;
 
-const mesh: Mesh<BoxGeometry, MeshNormalMaterial, Object3DEventMap> = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+    const scene: Scene = new THREE.Scene();
 
-const renderer: WebGLRenderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( width, height );
-renderer.setAnimationLoop( animation );
-document.body.appendChild( renderer.domElement );
+    const geometry: BoxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material: MeshNormalMaterial = new THREE.MeshNormalMaterial();
 
-// animation
-
-function animation( time: number ) {
-
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
-	renderer.render( scene, camera );
-
+    const mesh: Mesh<BoxGeometry, MeshNormalMaterial, Object3DEventMap> = new THREE.Mesh( geometry, material );
+    this.meshes.push(mesh);
+    scene.add( this.meshes[0] );
+    this.renderer.setSize( this.width, this.height );
+    this.renderer.setAnimationLoop( animation );
+    document.body.appendChild( this.renderer.domElement );
+  }
 }
-  }
-  }
-
