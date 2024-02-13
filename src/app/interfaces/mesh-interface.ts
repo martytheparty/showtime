74ee24c8bf2b3ef5a7bfab2be91207e7e3e5f8bf
Marty Trujillo <marty@ilikeemail.com>
2024-02-13 11:49:29 -0500

    Implements edit part 3

--- src/app/interfaces/mesh-interface.ts
+++ src/app/interfaces/mesh-interface.ts
@@ -1,4 +1,9 @@
+import { FormGroup } from "@angular/forms"
+import { Subscription } from "rxjs"
+
 export interface MeshInterface {
     id: number
     xPos: number
+    form?: FormGroup
+    sub?: Subscription
 }
--- src/app/toolbox/toolbox.component.ts
+++ src/app/toolbox/toolbox.component.ts
@@ -1,24 +1,27 @@
-import { Component, inject, effect } from '@angular/core';
+import { Component, inject, effect, OnDestroy } from '@angular/core';
 import { ThreejsService } from '../threejs.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MeshInterface } from '../interfaces/mesh-interface';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-toolbox',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './toolbox.component.html',
   styleUrl: './toolbox.component.scss'
 })
-export class ToolboxComponent{
+export class ToolboxComponent implements OnDestroy{
   threejsService: ThreejsService = inject(ThreejsService);
   xPosition = 0;
 
   editId = 0;
 
   meshList: MeshInterface[] = [];
+
+  subs: Subscription[] = [];
 
   constructor(){
     effect(
@@ -33,6 +36,12 @@ export class ToolboxComponent{
     ); 
   }
 
+  ngOnDestroy(): void {
+    this.subs.forEach(
+      (sub) => sub?.unsubscribe()
+    );
+  }
+
   addMesh(): void
   {
 
@@ -45,6 +54,20 @@ export class ToolboxComponent{
       }
     );
 
+    const sub: Subscription = form.valueChanges.subscribe(
+      () => {
+        if(form.value.xPos || form.value.xPos === 0)
+        {
+          // update...
+        }
+      }
+    );
+
+    this.subs.push(sub);
+    meshItem.sub = sub;
+
+    meshItem.form = form;
+
     if (this.xPosition === 0){
       this.xPosition = 2;
     } else if (this.xPosition > 0) {
@@ -54,9 +77,10 @@ export class ToolboxComponent{
     }
   }
 
-  deleteMesh(id: number): void
+  deleteMesh(meshItem: MeshInterface): void
   {
-    this.threejsService.deleteMesh(id);
+    this.threejsService.deleteMesh(meshItem.id);
+    meshItem.sub?.unsubscribe();
   }
 
   setEditId(id: number): void
