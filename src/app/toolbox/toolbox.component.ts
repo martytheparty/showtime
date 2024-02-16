a132cd7b9d53cd7c11ed444f554db16189921b87
Marty Trujillo <marty@ilikeemail.com>
2024-02-16 17:07:07 -0500

    Implements tabs

--- src/app/toolbox/toolbox.component.ts
+++ src/app/toolbox/toolbox.component.ts
@@ -1,16 +1,13 @@
 import { Component, inject, effect, OnDestroy } from '@angular/core';
 import { ThreejsService } from '../threejs.service';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MeshInterface } from '../interfaces/mesh-interface';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MeshManagerComponent } from './mesh-manager/mesh-manager.component';
 
 @Component({
   selector: 'app-toolbox',
   standalone: true,
-  imports: [MeshManagerComponent],
+  imports: [MatIconModule, MatTabsModule, MeshManagerComponent],
   templateUrl: './toolbox.component.html',
   styleUrl: './toolbox.component.scss'
 })
