054d52dc83d5b7ba8d3f466670627372bdfb7b86
Marty Trujillo <marty@ilikeemail.com>
2024-02-20 09:30:16 -0500

    Video 27 Starts Camera forms.

--- src/app/toolbox/camera-manager/camera-manager.component.ts
+++ src/app/toolbox/camera-manager/camera-manager.component.ts
@@ -2,20 +2,34 @@ import { Component, inject, effect } from '@angular/core';
 import { ThreejsService } from '../../threejs.service';
 import { CameraInterface } from '../../interfaces/camera-interface';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-camera-manager',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './camera-manager.component.html',
   styleUrl: './camera-manager.component.scss'
 })
 export class CameraManagerComponent {
   threeJsService: ThreejsService = inject(ThreejsService);
 
-  editing = false;
+  editing = true;
 
   cameraItem: CameraInterface | undefined;
+
+  form: FormGroup = new FormGroup({
+    fov: new FormControl(0),
+    aspect: new FormControl(0)
+  });
 
   constructor() {
     this.threeJsService.addMesh(
@@ -30,6 +44,11 @@ export class CameraManagerComponent {
     effect(
       () => {
         this.cameraItem = this.threeJsService.cameraItemValues();
+
+        this.form.setValue({
+          fov: this.cameraItem.fov,
+          aspect: this.cameraItem.aspect
+        });        
       }
     ); 
   }
