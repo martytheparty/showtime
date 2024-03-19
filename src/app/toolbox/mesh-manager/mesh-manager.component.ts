9391fd1572dcb4337149d5c9fe03defd6a46a132
Marty Trujillo <marty@ilikeemail.com>
2024-03-19 10:44:18 -0400

    Small bug fix and format tweak.

--- src/app/toolbox/mesh-manager/mesh-manager.component.ts
+++ src/app/toolbox/mesh-manager/mesh-manager.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ThreejsService } from '../../threejs.service';
-import { MeshInterface, } from '../../interfaces/mesh-interface';
+import { MeshInterface } from '../../interfaces/mesh-interface';
 import { Subscription } from 'rxjs';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
