bca2d4c8a1018f67b309c513f088457667cc1955
Marty Trujillo <marty@ilikeemail.com>
2024-06-27 18:39:27 -0400

    Video 156 - adds font loading indication.

--- src/app/toolbox/mesh-manager/font-list/font-list.component.ts
+++ src/app/toolbox/mesh-manager/font-list/font-list.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
+import { FontName } from '../../../interfaces/mesh-interface';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-font-list',
   standalone: true,
   imports: [ 
-    MatSelectModule
+    MatSelectModule,
+    MatIconModule
   ],
   templateUrl: './font-list.component.html',
   styleUrl: './font-list.component.scss'
 })
 export class FontListComponent {
+
+  fonts: FontName[] = [];
 
 }
